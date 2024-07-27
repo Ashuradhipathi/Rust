d94139be86b9890f2af4d0579c1c8de94fb2aa30
Prem Kiran Laknaboina <premlaknaboina@gmail.com>
2024-07-27 16:56:48 +0000

    4th chapter

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -110,7 +110,7 @@ fn calculate_length(s: String) -> (String, usize) {
 
     (s, length)
 }*/
-fn main(){
+/*fn main(){
     let mut s1 = String::from("Heyy");
 
     let len = calculate_length(&mut s1);
@@ -138,4 +138,46 @@ fn main(){
 fn calculate_length(s: &mut String) -> usize {
     s.push_str(", World!");
     s.len()
+}*/
+fn main(){
+    let mut my_string = String::from("hello world");
+
+  // `first_word` works on slices of `String`s, whether partial or whole
+  let word = first_word(&my_string[0..6]);
+  let word = first_word(&my_string[..]);
+  // `first_word` also works on references to `String`s, which are equivalent
+  // to whole slices of `String`s
+  let word = first_word(&my_string);
+
+  let my_string_literal = "hello world";
+
+  // `first_word` works on slices of string literals, whether partial or whole
+  let word = first_word(&my_string_literal[0..6]);
+  let word = first_word(&my_string_literal[..]);
+
+  // Because string literals *are* string slices already,
+  // this works too, without the slice syntax!
+  let word = first_word(my_string_literal);
+
+    println!("{word}");
+
+
+    let a = [1,2,3,4,5];
+
+    let slice = &a[1..4];
+
+assert_eq!(slice, &[2,3,4]);
+
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
