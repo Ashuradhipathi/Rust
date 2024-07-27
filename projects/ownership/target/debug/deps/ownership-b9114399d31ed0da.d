1e2f4c326d7a65b64bb0d9059edf4bd0b9d58adb
Prem Kiran Laknaboina <premlaknaboina@gmail.com>
2024-07-27 09:06:00 +0000

    chapter 4.1

---
+++ projects/ownership/target/debug/deps/ownership-b9114399d31ed0da.d
@@ -0,0 +1,5 @@
+/workspaces/Rust/projects/ownership/target/debug/deps/ownership-b9114399d31ed0da: src/main.rs
+
+/workspaces/Rust/projects/ownership/target/debug/deps/ownership-b9114399d31ed0da.d: src/main.rs
+
+src/main.rs:
---
+++ projects/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/workspaces/Rust/projects/ownership/target/debug/ownership: /workspaces/Rust/projects/ownership/src/main.rs
