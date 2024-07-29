914066000276b92f6d51aa6507663416c6147b8d
Prem Kiran Laknaboina <premlaknaboina@gmail.com>
2024-07-29 18:15:27 +0000

    cahpter 5

--- projects/rectangle/src/main.rs
+++ projects/rectangle/src/main.rs
@@ -5,21 +5,55 @@ struct Rectangle{
 }
 
 fn main() {
-    let scale = 2;
+    //let scale = 2;
     let rect1 = Rectangle{
-        width:dbg!(scale*30),
+        //width:dbg!(scale*30),
+        width:30,
         height:50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let sq = Rectangle::square(3);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
 
     println!(
         "The area of rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
-    println!("{rect1:#?}");
-    dbg!(&rect1);
+    if rect1.width() {
+        println!("Width of rectangle is greater than 0")
+    }
+
+    //println!("{rect1:#?}");
+    //dbg!(&rect1);
 }
 
-fn area(rect:&Rectangle) -> u32 {
-    rect.width * rect.height
+impl Rectangle{
+
+fn width(&self) -> bool {
+    self.width > 0
+}
+fn area(&self) -> u32 {
+    self.width * self.height
+}
+fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+}
+
+fn square(size: u32) -> Self {
+    Self {
+        width: size,
+        height: size,
+    }
+}
 }
